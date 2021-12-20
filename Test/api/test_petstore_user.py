d68e70068db01e29760b0fb1eb456e720e45cad5
Felipe Brum <felipe.brum@engenharia.ufjf.br>
2021-12-20 14:42:21 -0300

    Criação dos testes da API da PetStore, entidade User, métodos POST, GET , PUT e DELETE

--- Test/api/test_petstore_user.py
+++ Test/api/test_petstore_user.py
@@ -65,3 +65,59 @@ def testar_consultar_usuario():
     assert corpo_da_resposta['username'] == username
     assert corpo_da_resposta['email'] == email
     assert corpo_da_resposta['phone'] == phone
+
+# O teste de ALTERAR INFORMAÇÕES (PUT):
+def testar_alterar_usuario():
+    #1- Configurar
+    username = 'minduim'
+    status_code_esperado = 200
+    codigo_esperado = 200
+    tipo_esperado = "unknown"
+    mensagem_esperada = '1001'
+
+    #2- Executar
+    resposta = requests.put(
+        url=f'{base_url}/{username}',
+        data=open('C:\\Users\\aless\\PycharmProjects\\Cursos_Iterasys\\FTS132\\Test\\db\\user2.json', 'rb'),   # O body da mensagem que vem do json
+        headers=headers,                                                                                       # O header com o Content-Type
+        )
+
+    #3- Formatar
+    corpo_da_resposta = resposta.json()         # Formata como Json (pois inicialmente ele vem numa linha unica)
+    print(resposta)                             # Resposta Bruta
+    print(resposta.status_code)                 # Resposta Code
+    print(corpo_da_resposta)                    # Resposta Formatada
+
+    #4- Validar
+    assert resposta.status_code == status_code_esperado
+    assert corpo_da_resposta['code'] == codigo_esperado
+    assert corpo_da_resposta['type'] == tipo_esperado
+    assert corpo_da_resposta['message'] == mensagem_esperada
+
+# O teste de ALTERAR INFORMAÇÕES (PUT):
+def testar_excluir_usuario():
+    #1- Configurar
+    username = 'minduim'
+    status_code_esperado = 200
+    codigo_esperado = 200
+    tipo_esperado = "unknown"
+    mensagem_esperada = 'minduim'
+
+    #2- Executar
+    resposta = requests.delete(
+        url=f'{base_url}/{username}',
+        data=open('C:\\Users\\aless\\PycharmProjects\\Cursos_Iterasys\\FTS132\\Test\\db\\user1.json', 'rb'),   # O body da mensagem que vem do json
+        headers=headers,                                                                                       # O header com o Content-Type
+        )
+
+    #3- Formatar
+    corpo_da_resposta = resposta.json()         # Formata como Json (pois inicialmente ele vem numa linha unica)
+    print(resposta)                             # Resposta Bruta
+    print(resposta.status_code)                 # Resposta Code
+    print(corpo_da_resposta)                    # Resposta Formatada
+
+    #4- Validar
+    assert resposta.status_code == status_code_esperado
+    assert corpo_da_resposta['code'] == codigo_esperado
+    assert corpo_da_resposta['type'] == tipo_esperado
+    assert corpo_da_resposta['message'] == mensagem_esperada

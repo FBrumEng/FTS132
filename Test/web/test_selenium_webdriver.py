814f656b73c031ab7a0ed8671384e0f432596490
Felipe Brum <felipe.brum@engenharia.ufjf.br>
2022-01-05 10:09:11 -0300

    Importação e config do script gerado no selenium IDE. Criação manual do mesmo script no Webdriver. Uso de uma lista de dados como script de compra de curso. Screenshots obtidos.

--- Test/web/test_selenium_webdriver.py
+++ Test/web/test_selenium_webdriver.py
@@ -1,8 +1,13 @@
 # 1- Importar Bibliotecas
+import os
+from datetime import datetime
 from selenium import webdriver
 from selenium.webdriver import Keys
 from selenium.webdriver.common.by import By
 import pytest
+
+caminho_print = 'C:/Users/aless/PycharmProjects/Cursos_Iterasys/FTS132/prints/' \
+                + datetime.now().strftime('%Y-%m-%d %H-%M-%S') + '/'
 
 # 2- Classe
 class Test_Selenium_Webdriver:
@@ -11,8 +16,14 @@ class Test_Selenium_Webdriver:
     def setup_method(self):
         # Declarar o objeto do Selenium e instanciar como o navegador desejado
         self.driver = webdriver.Chrome('C:/Users/aless/PycharmProjects/Cursos_Iterasys/FTS132/drivers/chrome/versao_96/chromedriver.exe')
-        self.driver.implicitly_wait(30) # O Selenium vai esperar até 3 segs pelos elementos
+        self.driver.implicitly_wait(40) # O Selenium vai esperar até 3 segs pelos elementos
         self.driver.maximize_window()  # Maximizar a janela do navegador
+
+    # Criar a pasta com data e hora para guardar os prints
+    try:
+        os.mkdir(caminho_print)
+    except:
+        print('A pasta já existia')
 
     # Definição de Fim - Executa depois do teste
     def teardown_method(self):
@@ -75,20 +86,22 @@ class Test_Selenium_Webdriver:
         assert self.driver.find_element(By.CSS_SELECTOR, 'span.new-price').text == 'R$ 199,00'
 
     # Data driven tests - a partir da massa de dados criada
-    @pytest.mark.parametrize('termo, curso, preco',[
-        ('mantis', 'Mantis', 'R$ 59,99'),
-        ('ctfl', 'Preparatório CTFL', 'R$ 199,00'),
+    @pytest.mark.parametrize('id, termo, curso, preco',[
+        ('1', 'mantis', 'Mantis', 'R$ 59,99'),
+        ('2', 'ctfl', 'Preparatório CTFL', 'R$ 199,00'),
     ])
 
-    def testar_comprar_curso_mantis_com_click_na_lupa(self, termo, curso, preco):
+    def testar_comprar_curso_mantis_com_click_na_lupa(self, id, termo, curso, preco):
         # O Selenium abre a url indicada - site alvo do teste
         self.driver.get('https://www.iterasys.com.br')
+        self.driver.get_screenshot_as_file(f'{caminho_print}teste {id}_passo1_home.png')
         # O Selenium clica na caixa de pesquisa
         self.driver.find_element(By.ID, 'searchtext').click()
         # O Selenium apaga o conteúdo da caixa de pesquisa (bug)
         self.driver.find_element(By.ID, 'searchtext').clear()
         # O Selenium escreve 'mantis' na caixa de pesquisa
         self.driver.find_element(By.ID, 'searchtext').send_keys(termo)
+        self.driver.get_screenshot_as_file(f'{caminho_print}teste {id}_passo2_pesquisa_pelo_curso.png')
         # O Selenium clica no botão da Lupa
         self.driver.find_element(By.ID, 'btn_form_search').click()
         # O Selenium clica em matricule-se
